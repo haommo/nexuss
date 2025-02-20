4c0bdf0f4b591dabfc0a1384af5facfab893568a
haommo <137039939+haommo@users.noreply.github.com>
2025-02-20 23:21:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
+sudo fallocate -l 16G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && sudo swapon --show && free -h
 sudo apt install -y build-essential pkg-config libssl-dev git-all protobuf-compiler
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
