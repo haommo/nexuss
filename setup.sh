bc1f565093ffc4d51e87633dd32b39ae43da2911
haommo <137039939+haommo@users.noreply.github.com>
2025-02-21 00:18:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo fallocate -l 16G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && sudo swapon --show && free -h
 sudo apt install -y build-essential pkg-config libssl-dev git-all protobuf-compiler
-source $HOME/.cargo/env
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 rustup target add riscv32i-unknown-none-elf
-curl -s https://cli.nexus.xyz/ | sh
+yes | curl -s https://cli.nexus.xyz/ | sh
