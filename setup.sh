8c70de84db0fca15dbe324489afbe169902499a8
haommo <137039939+haommo@users.noreply.github.com>
2025-02-20 23:19:24 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential pkg-config libssl-dev git-all protobuf-compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup target add riscv32i-unknown-none-elf
+yes | curl -s https://cli.nexus.xyz/ | sh
